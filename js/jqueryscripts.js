ccd8164f87b773ff9bccfd71cd44f74585809ea4
scbarcelonaconsulting <sc.barcelona.consulting@gmail.com>
2016-03-02 20:18:53 +0100

    2016-03-02

---
+++ js/jqueryscripts.js
@@ -0,0 +1,20 @@
+// el cabezal se encoje al hacer scroll
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 40){  
+    	$('#header').addClass("shrink");
+  		}
+  	else{
+    	$('#header').removeClass("shrink");
+  		}
+});
+
+
+// menú para móbil
+
+$(function() {
+	$("#openmenu").click(function() {
+		$("#menu").toggleClass("opened");
+		$("#openmenu").toggleClass("close");
+	});
+});
